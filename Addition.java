d7912a205abed4bb391ec181e280564eb4929393
ManiBansal0025 <44293399+ManiBansal0025@users.noreply.github.com>
2019-07-23 08:11:04 +0530

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,13 @@
+/*Write a program to add an integer variable having value
+5 and a double variable having value 6.2.*/
+
+class addition 
+{
+	public static void main(String[] args) 
+	{
+		double d = 6.2d;
+		double a = (double)5;
+		double c = d+a;
+		System.out.println(c);
+	}
+}
