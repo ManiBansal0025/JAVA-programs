6f61ab9be2743ebca315aaa4d2b81f2414e9b64a
ManiBansal0025 <44293399+ManiBansal0025@users.noreply.github.com>
2019-11-10 09:23:00 +0530

    Create Wrapperdemo1.java

---
+++ Wrapperdemo1.java
@@ -0,0 +1,18 @@
+/*Create objects of all the wrapper classes 
+and print then on console, with using constructor.*/
+class  Wrapper1
+{
+	public static void main(String[] args) 
+	{
+		Integer a = new Integer(300);
+		Float  a1 = new Float(36.4);
+		Character a2 = new Character('m');
+		Byte a3 = new Byte(2);
+		Double a4 = new Double(367.75);
+		System.out.println(a);
+		System.out.println(a1);
+		System.out.println(a2);
+		System.out.println(a3);
+		System.out.println(a4);
+	}
+}
