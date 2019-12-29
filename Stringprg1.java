ee84bd44cf18ee815f5d3abbb6d248fe52308ddd
ManiBansal0025 <44293399+ManiBansal0025@users.noreply.github.com>
2019-12-29 09:17:38 +0530

    Create Stringprg1.java

---
+++ Stringprg1.java
@@ -0,0 +1,13 @@
+/*Write a program to print a string entered by 
+user.*/
+import java.util.Scanner;
+class stringprg1
+{
+	public static void main(String args[])
+	{
+		Scanner sc = new Scanner(System.in);
+		String var = sc.next();
+		System.out.println(var);
+	}
+
+}
